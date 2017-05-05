f07813aaa8a1ff7f3eee2cde3ca6342286bd1984
Jessica Hollingshead <Jessica Hollingshead>
2017-05-05 16:09:05 -0600

    finished product!

--- public/app/routes/inspiration/inspirationCtrl.js
+++ public/app/routes/inspiration/inspirationCtrl.js
@@ -1,3 +1,19 @@
-angular.module('app').controller('inspirationCtrl', function($scope){
+angular.module('app').controller('inspirationCtrl', function($scope, inspirationService){
+
+
+
+    $scope.getPics = function(){
+        inspirationService.getPics().then(function(response){
+            console.log(response);
+            $scope.pictures = response.data;
+        })
+    }
+
+    $scope.getPics();
+
+    $scope.show= false;
+    $scope.menu = function(){
+        $scope.show = !$scope.show;
+    }
     
 })
--- public/app/routes/inspiration/inspirationService.js
+++ public/app/routes/inspiration/inspirationService.js
@@ -1,3 +1,17 @@
 angular.module('app').service('inspirationService', function($http){
     
+    this.getPics = function(){
+        return $http({
+            method: 'GET',
+            url: "https://api.unsplash.com/photos/curated?client_id=f857da4f8c32145c72b0031898d5348d3663b69dd29e2d08339d7cde4707ce2a",
+        })
+    }
+
 })
+
+
+
+
+// 035ba13eaf95fedf5e7b0ccb7eae865ff1470019bac6d95e3ff3d65781593f23
+
+// 11d016f5127852e024602ce1c0e6794e0961c6f2d7e8a53897f2a373f1df6c01
